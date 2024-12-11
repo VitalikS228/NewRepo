4b3741bacf750223a20e81bf9bd42a451618bd1f
Vitalii <senchenkov228@gmail.com>
2024-12-11 02:49:32 +0200

    Added is_power_of_two function

--- utils.py
+++ utils.py
@@ -6,3 +6,12 @@ def is_prime(num):
     for i in range(2, int(num**0.5) + 1):
         if num % i == 0: return False
     return True
+<<<<<<< Updated upstream
+=======
+def is_power_of_five(n):
+    if n < 1: return False
+    while n % 5 == 0: n //= 5
+    return n == 1
+def is_power_of_two(n):
+    return n > 0 and (n & (n - 1)) == 0
+>>>>>>> Stashed changes
