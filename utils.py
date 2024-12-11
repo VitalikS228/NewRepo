f992e1e20791c14cecc2a44b1460c7de5af73ba5
Vitalii <senchenkov228@gmail.com>
2024-12-11 02:49:27 +0200

    Added utils.py and main.py with basic functions

---
+++ utils.py
@@ -0,0 +1,8 @@
+def factorial(n):
+    return 1 if n == 0 else n * factorial(n - 1)
+
+def is_prime(num):
+    if num < 2: return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0: return False
+    return True
