311999d00497337790832301c0c295caac968d47
Vitalii <senchenkov228@gmail.com>
2024-12-11 02:49:30 +0200

    Added is_power_of_five function

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
 from utils import factorial
 print(factorial(5))
+from utils import is_power_of_five
+print(is_power_of_five(25))
--- utils.py
+++ utils.py
@@ -6,3 +6,7 @@ def is_prime(num):
     for i in range(2, int(num**0.5) + 1):
         if num % i == 0: return False
     return True
+def is_power_of_five(n):
+    if n < 1: return False
+    while n % 5 == 0: n //= 5
+    return n == 1
